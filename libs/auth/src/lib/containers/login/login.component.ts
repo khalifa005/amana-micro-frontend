46bc37e7a0bd3fb963fc2c8774ab0f54fbddecd4
mahmoud khalifa <mahmudkhalifa12@outlook.com>
2023-08-12 14:36:52 +0300

    add fake server

--- libs/auth/src/lib/containers/login/login.component.ts
+++ libs/auth/src/lib/containers/login/login.component.ts
@@ -1,10 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy  } from '@angular/core';
 
 @Component({
   selector: 'amana-micro-frontend-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
+
+// Change the ChangeDetectionStrategy to OnPush
+// Now that we are using the presentation and container component pattern and we know that we only need to check the child components for changes if a DOM event or a @Input or @Output passes new primitives or reference values. In this way we can tell Angular not check the whole component tree which can cause performance issues in larger applications.
+
 export class LoginComponent implements OnInit {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   constructor() {}
