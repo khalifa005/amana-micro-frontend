d9e7bf4b9ad25e09f90da76809dfc3912c30cf0b
mahmoud khalifa <mahmudkhalifa12@outlook.com>
2023-08-12 17:13:02 +0300

    Add logic to subscribe to User Subject in the Auth

--- libs/auth/src/index.ts
+++ libs/auth/src/index.ts
@@ -1,3 +1,3 @@
 export * from './lib/auth.module';
-
 export * from './lib/containers/login/login.component';
+export { AuthService } from './lib/services/auth/auth.service';
--- libs/layout/src/lib/containers/layout/layout.component.ts
+++ libs/layout/src/lib/containers/layout/layout.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { AuthService } from '@amana-micro-frontend/auth';
+import { User } from '@amana-micro-frontend/data-models';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'amana-micro-frontend-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css'],
 })
-export class LayoutComponent {}
+export class LayoutComponent implements OnInit {
+  user$!: Observable<User>;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.user$ = this.authService.user$;
+  }
+}
