d2dee0982b58a95e74190630f199b7a0a9b2f674
mahmoud khalifa <mahmudkhalifa12@outlook.com>
2023-08-12 17:08:36 +0300

    Added a BehaviorSubject to the Auth service

--- libs/auth/src/lib/services/auth/auth.service.ts
+++ libs/auth/src/lib/services/auth/auth.service.ts
@@ -2,17 +2,20 @@ import { User } from './../../../../../data-models/src/lib/User';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IAuthenticate } from 'libs/data-models/src/lib/IAuthenticate';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private userSubject$ = new BehaviorSubject<any>(null);
+  user$ = this.userSubject$.asObservable();
 
   constructor(private httpClient: HttpClient) {}
 
   login(authenticate: IAuthenticate): Observable<User> {
-    return this.httpClient.post<User>('http://localhost:3000/login', authenticate);
+    return this.httpClient.post<User>('http://localhost:3000/login', authenticate)
+    .pipe(tap((user: User) => this.userSubject$.next(user)));
   }
 }
