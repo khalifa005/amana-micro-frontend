484d066e6b343b413a5ade45cf466f40e90e12b8
mahmoud khalifa <mahmudkhalifa12@outlook.com>
2023-08-30 23:03:56 +0300

    imported icons

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { AuthModule, authRoutes } from '@amana-micro-frontend/auth';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@amana-micro-frontend/material';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
     LayoutModule,
     AuthModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
